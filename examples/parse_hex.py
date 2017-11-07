a4bde9b1eeba0afb392d7ed24f7cdb868ad337b5
Martin Peters <martin@freakybytes.net>
2017-11-07 13:59:14 +0100

    added example dir, with one example

---
+++ examples/parse_hex.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import baos_knx_parser as knx
+
+"""
+This example shows, how to take the complete hex from the logs and parse all standard fields
+"""
+
+
+print("00:04:01        2012-02-27      3.2.50  2/3/4   00800C7E        2900BCC0323213040200800C7E")
+t = knx.parse_knx_telegram(bytes.fromhex('2900BCC0323213040200800C7E'))
+print(t)
+print(t.apci)
+print()
+
+
+print("00:06:10        2012-02-27      3.6.39  2/2/232 00800C7E        2900BCE0362712E80200800C7E")
+t = knx.parse_knx_telegram(bytes.fromhex('2900BCE0362712E80200800C7E'))
+print(t)
+print(t.apci)
+
+
+print()
+print("00:06:12        2012-02-27      3.5.37  2/2/4   0081    2900BCC035251204000081")
+t = knx.parse_knx_telegram(bytes.fromhex('2900BCC035251204000081'))
+print(t)
+print(t.apci)
