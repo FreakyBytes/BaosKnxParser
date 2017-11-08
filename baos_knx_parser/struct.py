9b139a9df44317cbf06c3f143a642474e6ba1391
Martin Peters <martin@freakybytes.net>
2017-11-08 15:22:21 +0100

    optimized structs

--- baos_knx_parser/struct.py
+++ baos_knx_parser/struct.py
@@ -1,14 +1,17 @@
 import bitstruct
 
-CEMI_MSG_CODE = bitstruct.compile('>u8')
-CEMI_ADD_LEN = bitstruct.compile('>u8')
+STD_U16 = bitstruct.compile('>u16')
+STD_U8 = bitstruct.compile('>u8')
+
+CEMI_MSG_CODE = STD_U8
+CEMI_ADD_LEN = STD_U8
 
 KNX_CTRL = bitstruct.compile('>u2b1b1u2b1b1')
 KNX_CTRLE = bitstruct.compile('>b1u3u4')
 KNX_ADDR_PHYSICAL = bitstruct.compile('>u4u4u8')
 KNX_ADDR_GROUP = bitstruct.compile('>u5u3u8')
 KNX_DRL = bitstruct.compile('>b1u3u4')
-KNX_LENGTH = bitstruct.compile('>u8')
+KNX_LENGTH = STD_U8
 
 KNX_APCI = bitstruct.compile('>p6u10')
 KNX_TPCI = bitstruct.compile('>u2p4p2')
